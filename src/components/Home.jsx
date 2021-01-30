b17868a85793851aa987c4a3b2199b6a3e222fbd
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2021-01-30 23:00:32 +0200

    Home Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,21 @@
+import { Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    home: {
+        padding: "1rem",
+    },
+}));
+
 function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
+    const classes = useStyles();
+
+    return (
+        <>
+            <Paper className={classes.home}>
+                <Typography>A simple project created with React.</Typography>
+            </Paper>
+        </>
+    );
 }
 export default Home;
