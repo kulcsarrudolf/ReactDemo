1549b4b095cd9156539dd878e6b4d03a99d979e8
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2021-02-05 16:20:32 +0200

    Reset user fixed

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,14 +1,17 @@
 export const setUser = (user) => (dispatch) => {
-    dispatch({
-        type: "SET_USER",
-        payload: {
-            user: user,
-        },
-    });
+  dispatch({
+    type: "SET_USER",
+    payload: {
+      user: user,
+    },
+  });
 };
 
 export const resetUser = () => (dispatch) => {
-    dispatch({
-        type: "RESET_USER",
-    });
+  dispatch({
+    type: "RESET_USER",
+    payload: {
+      user: null,
+    },
+  });
 };
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,14 +1,14 @@
 const initState = "Kulcsar Rudolf";
 
 const userReducer = (state = initState, action) => {
-    switch (action.type) {
-        case "SET_USER":
-            return action.payload.user;
-        case "RESET_USER":
-            return initState;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "SET_USER":
+      return action.payload.user;
+    case "RESET_USER":
+      return action.payload.user;
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
