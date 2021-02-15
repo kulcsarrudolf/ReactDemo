b3a8d7f06159edebf609974a7540cb4a408097a4
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2021-02-15 23:11:18 +0200

    Material UI Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./Navbar";
 import Main from "./Main";
+import { Container, CssBaseline } from "@material-ui/core";
 
 function App() {
     return (
         <>
-            <Navbar />
-            <Main />
+            <CssBaseline />
+            <Container>
+                <Navbar />
+                <Main />
+            </Container>
         </>
     );
 }
